9c2399b03609a6b0416045b015d025a70d8423e9
Albert Tieu <albert.tieu@mrjaffesclass.com>
2022-09-26 14:16:49 -0700

    init commit

---
+++ forLoops/forLoops.java
@@ -0,0 +1,15 @@
+
+public class forLoops
+{
+    public static void main(String[]args) {
+        for (int i = 0; i <= 15; i++) {
+            System.out.println("i am " + i);
+        }
+        for (int j = 10; j != 0; j = j - 2) {
+            System.out.println("j is " + j);
+        }
+        for (int k = 10; k >= 0; k = k - 3) {
+            System.out.println("k is " + k);
+        }
+    }
+}
